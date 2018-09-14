ddf6f3569485dd192cb77924acec21b4ce7f4e77
주형준 <nighthawk189@gmail.com>
2018-09-14 14:42:42 +0900

    Version 2를 위한 Unity 세팅 업로드

---
+++ HomuHomu_v2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    private static GameManager _gameManager = null;
+    public static GameManager CurrentGameManager {
+        get { return _gameManager; }
+    }
+}
