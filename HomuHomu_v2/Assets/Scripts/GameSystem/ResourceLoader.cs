8c017524eacc8feb4397917b7a27aaba5bb12fd9
주형준 <nighthawk189@gmail.com>
2018-09-14 15:22:03 +0900

    made Default Scripts

---
+++ HomuHomu_v2/Assets/Scripts/GameSystem/ResourceLoader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class ResourceLoader {
+    private static ResourceLoader _loader = null;
+    public static ResourceLoader Loader {
+        get {
+            if (_loader == null) {
+                _loader = new ResourceLoader();
+            }
+            return _loader;
+        }
+    }
+
+    public ResourceLoader() {
+        //load default info
+    }
+
+}
---
+++ HomuHomu_v2/Assets/Scripts/Player/PlayerMoveController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Player { 
+    public class PlayerMoveController : MonoBehaviour
+    {
+        private static PlayerMoveController _moveController;
+        public static PlayerMoveController MoveController {
+            get { return _moveController; }
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
